55d49594a7677e6459784cab767041aea94bbbac
Rafael <rafaelfelipejudoca@gmail.com>
2026-04-06 16:18:18 -0300

    estrutura

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+require('./startup/db')(); // Conectar banco
+require('./startup/router')(app); // Rodar o router
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
